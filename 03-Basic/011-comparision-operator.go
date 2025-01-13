bd92f80f6c5bb0227c17d3d7817aea5e0fcdf759
F4121 <muhamadfaizi246@gmail.com>
2025-01-13 22:58:13 +0700

    comparision operator

---
+++ 03-Basic/011-comparision-operator.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 10
+	var b = 11
+
+	// Greater than ">"
+	fmt.Println(a > b)
+	// Output: false
+
+	// Less than "<"
+	fmt.Println(a < b)
+	// Output: true
+
+	// More than equal to
+	fmt.Println(a >= b)
+	// Output: false
+
+	// Less than equal to
+	fmt.Println(a <= b)
+	// Output: true
+
+	// Equal
+	fmt.Println(a == b)
+	// Output: false
+
+	// Not equal
+	fmt.Println(a != b)
+	// Output: true
+}
