181f416af1f46c91cccb35f48a433468656a0f64
F4121 <muhamadfaizi246@gmail.com>
2025-01-13 22:08:25 +0700

    numbers

---
+++ 03-Basic/002-data-number.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+// reference for type data number https://hostman.com/tutorials/data-types-in-go/
+
+func main() {
+	fmt.Println("Satu =", 1)
+	fmt.Println("Dua =", 2)
+	fmt.Println("Tiga koma lima =", 3.5)
+}
